3e74d4b40a049d7ca13ffdbdc9ad3d22d78e0a1f
agencyadmin <osmprobox@gmail.com>
2022-01-23 21:35:17 +0300

    linq changed

--- Program.cs
+++ Program.cs
@@ -2,55 +2,60 @@
 using System.IO;
 using System.Linq;
 
-class Program
-
+namespace FileScaner
 {
-    static void Main(string arg)
+    class Program
+
     {
-        string TargetFolder = arg;// @"C:\Users\wmtra\Desktop\TestDir\Modul8_Task1"; 
-
-        try
+        static void Main(string[] args)
         {
-            if (Directory.Exists(TargetFolder))
-            {
+            string TargetFolder = @"C:\Users\wmtra\Desktop\TestDir\Modul8_Task1";  //args[0]; 
 
-                string[] directories = Directory.GetDirectories(TargetFolder);
-                foreach (string d in directories) Console.WriteLine(d);
-                foreach (string d in directories)// (where d:Directory(TargetFolder) => d.GetLastAccessTimeUTC <= DateTime.Now + DataTime.Hour(3) ))
+            try
+            {
+                if (Directory.Exists(TargetFolder))
                 {
-                    if (Directory.GetLastAccessTime(d) < DateTime.Now.AddMinutes(30))
+
+                    string[] directories = Directory.GetDirectories(TargetFolder);
+                    foreach (string d in directories) Console.WriteLine(d);
+                    foreach (string d in directories)// (Where d:Directory => d.GetLastAccessTime <= DateTime.Now.AddMinutes(30) ))
                     {
-                        Directory.Delete(d, true);
-                        Console.WriteLine("Папка " + d + " использовалась: " + Directory.GetLastAccessTime(d) + " и будет удалена через: " + (DateTime.Now - Directory.GetLastAccessTimeUtc(d)));
-                        Console.WriteLine("Папка " + d + " удалена: " + DateTime.Now);
-                        DateTime date1 = DateTime.Now;
-                        DateTime date2 = Directory.GetLastAccessTime(d);
-                        TimeSpan interval = date1 - date2;
-                        Console.WriteLine($"Крайний доступ: {0} - Время текущее: {1} = Осталось до удаления дней.часов.минут.секунд: {2}", date2, date1, interval.ToString());
+                        if (Directory.GetLastAccessTime(d) < DateTime.Now.AddMinutes(30))
+                        {
+                            Directory.Delete(d, true);
+                            Console.WriteLine("Папка " + d + " использовалась: " + Directory.GetLastAccessTime(d) + " и будет удалена через: " + (DateTime.Now - Directory.GetLastAccessTimeUtc(d)));
+                            Console.WriteLine("Папка " + d + " удалена: " + DateTime.Now);
+                            DateTime date1 = DateTime.Now;
+                            DateTime date2 = Directory.GetLastAccessTime(d);
+                            TimeSpan interval = date1 - date2;
+                            Console.WriteLine($"Крайний доступ: {0} - Время текущее: {1} = Осталось до удаления дней.часов.минут.секунд: {2}", date1, date2, interval.ToString());
+                        }
                     }
-                }
-                string[] files = Directory.GetFiles(TargetFolder);
-                foreach (string f in files) Console.WriteLine(f);
-                foreach (string d in files) //(where d: Directory (TargetFolder) => d.GetLastAccessTimeUTC <= DateTime.Now + DataTime.Hour(3) ))
-                {
-                    if (File.GetLastAccessTime(d).AddHours(3) < DateTime.Now.AddMinutes(30))
+                    string[] files = Directory.GetFiles(TargetFolder);
+                    foreach (string f in files) Console.WriteLine(f);
+                    foreach (string d in files) //(Where d => d.GetLastAccessTime <= DateTime.Now.AddMinutes(30) ))
                     {
+                        if (File.GetLastAccessTime(d) < DateTime.Now.AddMinutes(30))
+                        {
+                            
+                            Console.WriteLine("Файл " + d + " использовался: " + File.GetLastAccessTime(d) + " и будет удален через: " + (DateTime.Now - File.GetLastAccessTime(d)));
+                            
+                            DateTime date1 = DateTime.Now;
+                            DateTime date2 = File.GetLastAccessTime(d);
+                            TimeSpan interval = date1 - date2;
+                            Console.WriteLine($"Крайний доступ: {0} - Время текущее: {1} = Осталось до удаления дней.часов.минут.секунд: {2}", date1, date2, interval.ToString());
                         File.Delete(d);
-                        Console.WriteLine("Файл " + d + " использовался: " + File.GetLastAccessTime(d) + " и будет удален через: " + (DateTime.Now - File.GetLastAccessTime(d)));
-                        Console.WriteLine("Файл " + d + " удален:  " + DateTime.Now);
-                        DateTime date1 = DateTime.Now;
-                        DateTime date2 = File.GetLastAccessTime(d);
-                        TimeSpan interval = date1 - date2;
-                        Console.WriteLine($"Крайний доступ: {0} - Время текущее: {1} = Осталось до удаления дней.часов.минут.секунд: {2}", date2, date1, interval.ToString());
+                        Console.WriteLine("Файл " + d + " УДАЛЕН !!!!:  " + DateTime.Now);
+                        }
                     }
                 }
+
+                else throw new Exception("Такой папки не сущесвует! Или передан в программу не корректный путь! Или нет прав доступа к папке!");
             }
 
-            else throw new Exception("Такой папки не сущесвует! Или передан в программу не корректный путь! Или нет прав доступа к папке!");
+            catch (Exception ex)
+            { Console.WriteLine(ex); }
         }
 
-        catch (Exception ex)
-        { Console.WriteLine(ex); }
     }
-
 }
