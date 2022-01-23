cf8a406a696cf6448b96aceaf098838b83a9000b
agencyadmin <osmprobox@gmail.com>
2022-01-23 21:02:32 +0300

    task 2 call fo metod added доработано заание 1 - убрано утс время, вычитание времени от настоящего сделано

--- Program.cs
+++ Program.cs
@@ -18,14 +18,14 @@ class Program
                 foreach (string d in directories) Console.WriteLine(d);
                 foreach (string d in directories)// (where d:Directory(TargetFolder) => d.GetLastAccessTimeUTC <= DateTime.Now + DataTime.Hour(3) ))
                 {
-                    if (Directory.GetLastAccessTimeUtc(d).AddHours(3) < DateTime.Now.AddMinutes(30))
+                    if (Directory.GetLastAccessTime(d) < DateTime.Now.AddMinutes(30))
                     {
                         Directory.Delete(d, true);
-                        Console.WriteLine("Папка " + d + " использовалась: " + Directory.GetLastAccessTimeUtc(d) + " и будет удалена через: " + (DateTime.Now - Directory.GetLastAccessTimeUtc(d)));
+                        Console.WriteLine("Папка " + d + " использовалась: " + Directory.GetLastAccessTime(d) + " и будет удалена через: " + (DateTime.Now - Directory.GetLastAccessTimeUtc(d)));
                         Console.WriteLine("Папка " + d + " удалена: " + DateTime.Now);
                         DateTime date1 = DateTime.Now;
-                        DateTime date2 = Directory.GetLastAccessTimeUtc(d);
-                        TimeSpan interval = date2 - date1;
+                        DateTime date2 = Directory.GetLastAccessTime(d);
+                        TimeSpan interval = date1 - date2;
                         Console.WriteLine($"Крайний доступ: {0} - Время текущее: {1} = Осталось до удаления дней.часов.минут.секунд: {2}", date2, date1, interval.ToString());
                     }
                 }
@@ -33,14 +33,14 @@ class Program
                 foreach (string f in files) Console.WriteLine(f);
                 foreach (string d in files) //(where d: Directory (TargetFolder) => d.GetLastAccessTimeUTC <= DateTime.Now + DataTime.Hour(3) ))
                 {
-                    if (File.GetLastAccessTimeUtc(d).AddHours(3) < DateTime.Now.AddMinutes(30))
+                    if (File.GetLastAccessTime(d).AddHours(3) < DateTime.Now.AddMinutes(30))
                     {
                         File.Delete(d);
-                        Console.WriteLine("Файл " + d + " использовался: " + File.GetLastAccessTimeUtc(d) + " и будет удален через: " + (DateTime.Now - File.GetLastAccessTimeUtc(d)));
+                        Console.WriteLine("Файл " + d + " использовался: " + File.GetLastAccessTime(d) + " и будет удален через: " + (DateTime.Now - File.GetLastAccessTime(d)));
                         Console.WriteLine("Файл " + d + " удален:  " + DateTime.Now);
                         DateTime date1 = DateTime.Now;
-                        DateTime date2 = File.GetLastAccessTimeUtc(d);
-                        TimeSpan interval = date2 - date1;
+                        DateTime date2 = File.GetLastAccessTime(d);
+                        TimeSpan interval = date1 - date2;
                         Console.WriteLine($"Крайний доступ: {0} - Время текущее: {1} = Осталось до удаления дней.часов.минут.секунд: {2}", date2, date1, interval.ToString());
                     }
                 }
