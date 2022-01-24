0aa929508ab17fd386260d77fc166d1e3e0dc841
agencyadmin <osmprobox@gmail.com>
2022-01-24 10:33:51 +0300

    Переносим в отдельный метод задание 2 модуля 8

--- Program.cs
+++ Program.cs
@@ -22,13 +22,15 @@ namespace FileScaner
                     {
                         if (Directory.GetLastAccessTime(d) < DateTime.Now.AddMinutes(30))
                         {
-                            Directory.Delete(d, true);
+
                             Console.WriteLine("Папка " + d + " использовалась: " + Directory.GetLastAccessTime(d) + " и будет удалена через: " + (DateTime.Now - Directory.GetLastAccessTimeUtc(d)));
                             Console.WriteLine("Папка " + d + " удалена: " + DateTime.Now);
                             DateTime date1 = DateTime.Now;
                             DateTime date2 = Directory.GetLastAccessTime(d);
                             TimeSpan interval = date1 - date2;
                             Console.WriteLine($"Крайний доступ: {0} - Время текущее: {1} = Осталось до удаления дней.часов.минут.секунд: {2}", date1, date2, interval.ToString());
+                            Directory.Delete(d, true);
+                            Console.WriteLine("Папка " + d + " УДАЛЕНА! " + DateTime.Now);
                         }
                     }
                     string[] files = Directory.GetFiles(TargetFolder);
@@ -37,15 +39,15 @@ namespace FileScaner
                     {
                         if (File.GetLastAccessTime(d) < DateTime.Now.AddMinutes(30))
                         {
-                            
+
                             Console.WriteLine("Файл " + d + " использовался: " + File.GetLastAccessTime(d) + " и будет удален через: " + (DateTime.Now - File.GetLastAccessTime(d)));
-                            
+
                             DateTime date1 = DateTime.Now;
                             DateTime date2 = File.GetLastAccessTime(d);
                             TimeSpan interval = date1 - date2;
                             Console.WriteLine($"Крайний доступ: {0} - Время текущее: {1} = Осталось до удаления дней.часов.минут.секунд: {2}", date1, date2, interval.ToString());
-                        File.Delete(d);
-                        Console.WriteLine("Файл " + d + " УДАЛЕН !!!!:  " + DateTime.Now);
+                            File.Delete(d);
+                            Console.WriteLine("Файл " + d + " УДАЛЕН !!!!:  " + DateTime.Now);
                         }
                     }
                 }
